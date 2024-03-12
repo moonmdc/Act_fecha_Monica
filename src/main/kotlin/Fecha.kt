a3d5da8b989120e47c63c467c6c71daf3ae3a8f3
Moon <moonmdc@gmail.com>
2024-03-12 22:15:45 +0100

    Documentacion de la clase Facha

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -1,15 +1,34 @@
+/**
+ * Fecha
+ *
+ * @property dia
+ * @property mes
+ * @property a
+ * @constructor Create empty Fecha
+ */
 class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
 
 
+    /**
+     * Valida si la fecha representada por la instancia actual es válida.
+     *
+     *  @return retorna `true` si la fecha es válida, `false` de lo contrario.
+     */
     fun valida(): Boolean {
 
         if (dia < 1 || dia > 31) return false
         if (mes < 1 || mes > 12) return false
 
-        // determinamos la cantidad de días del mes:
        return diasMes()
-    } // … más métodos
+    }
 
+
+    /**
+     * Calcula la cantidad de días en el mes actual, considerando el año para febrero.
+     *
+     * @param diasdelMes
+     * @return retorna `true` si el día actual es válido en el mes actual, `false` de lo contrario.
+     */
     private fun diasMes():Boolean{
         var diasdelMes = 0
         when (mes) {
