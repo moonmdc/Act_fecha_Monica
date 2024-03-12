2041594016c9eff070d4c1b7a87a885e0c786420
Moon <moonmdc@gmail.com>
2024-03-12 19:50:56 +0100

    refactorizacion diaMes

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -7,6 +7,10 @@ class Fecha(private val d: Int, private val m: Int, private val a: Int) {
         if (m < 1 || m > 12) return false
 
         // determinamos la cantidad de días del mes:
+       return diasMes()
+    } // … más métodos
+
+    private fun diasMes():Boolean{
         var diasMes = 0
         when (m) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
@@ -14,5 +18,5 @@ class Fecha(private val d: Int, private val m: Int, private val a: Int) {
             2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
         return if (d > diasMes) false else true
-    } // … más métodos
+    }
 }
