45f323c9de786279fa21cc8503eb4e8e79ec392e
Moon <moonmdc@gmail.com>
2024-03-12 20:21:13 +0100

    Modificacion del nombre de la variable a diasdelMes

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -11,12 +11,12 @@ class Fecha(private val d: Int, private val m: Int, private val a: Int) {
     } // … más métodos
 
     private fun diasMes():Boolean{
-        var diasMes = 0
+        var diasdelMes = 0
         when (m) {
-            1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
-            4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+            1, 3, 5, 7, 8, 10, 12 -> diasdelMes = 31
+            4, 6, 9, 11 -> diasdelMes = 30
+            2 -> diasdelMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
-        return if (d > diasMes) false else true
+        return if (d > diasdelMes) false else true
     }
 }
