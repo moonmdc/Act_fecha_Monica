27c26350cc9464528978dda0ee8597d31bdb3a93
Moon <moonmdc@gmail.com>
2024-03-12 20:33:34 +0100

    Renombrar variables -dia, mes-

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -1,10 +1,10 @@
-class Fecha(private val d: Int, private val m: Int, private val a: Int) {
+class Fecha(private val dia: Int, private val mes: Int, private val a: Int) {
 
 
     fun valida(): Boolean {
 
-        if (d < 1 || d > 31) return false
-        if (m < 1 || m > 12) return false
+        if (dia < 1 || dia > 31) return false
+        if (mes < 1 || mes > 12) return false
 
         // determinamos la cantidad de días del mes:
        return diasMes()
@@ -12,11 +12,11 @@ class Fecha(private val d: Int, private val m: Int, private val a: Int) {
 
     private fun diasMes():Boolean{
         var diasdelMes = 0
-        when (m) {
+        when (mes) {
             1, 3, 5, 7, 8, 10, 12 -> diasdelMes = 31
             4, 6, 9, 11 -> diasdelMes = 30
             2 -> diasdelMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
-        return if (d > diasdelMes) false else true
+        return if (dia > diasdelMes) false else true
     }
 }
